e8274e198df358c2d88e92a49e1ad025e4847c6a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-29 20:13:54 -0800

    it works!

--- wordcount.py
+++ wordcount.py
@@ -1,2 +1,19 @@
 # put your code here.
-# no actual change
+
+def word_counter(file):
+    filename = open(file)
+    counter_dict = {}
+    for line in filename:
+        line = line.rstrip()
+        line = line.split(" ")
+
+        for word in line:
+            if word in counter_dict:
+                counter_dict[word] += 1
+            else:
+                counter_dict[word] = 1
+    for word, counter in counter_dict.items():
+        print(word, ': ', counter)
+
+word_counter('test.txt')
+    
